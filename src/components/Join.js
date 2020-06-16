2e1f465cdb0cc254b1f2171883664c3eb054c0d7
Antonio da Paixao <aquilesdapaixao@gmail.com>
2020-06-16 09:23:39 +0100

    initial commit

---
+++ src/components/Join.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Link from 'react-router-dom';
+
+const Join = () => {
+
+    const [name, setName ] = useState('');
+    const [room, setRoom] = useState('');
+    return (
+        <div className="joinContainer"></div>
+    );
+}
+
+export default Join;
